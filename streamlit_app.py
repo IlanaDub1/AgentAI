2c920cf81acfb659a4790aeb17aa56e091d82686
IlanaDub1 <61084401+IlanaDub1@users.noreply.github.com>
2025-05-19 09:58:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -328,14 +328,12 @@ def page_result():
             # Save feedback to database
             save_result(feedback, datetime.now(), st.session_state.user_email, st.session_state.session_id)
 
+     # הוספת קישור אחרי הצגת המשוב
+            st.markdown("""
+            ---
+            📌 [תודה לכם על ההשתפות בסימולציה, הקליקו פה כדי לענות על שאלון](https://telavivmedicine.fra1.qualtrics.com/jfe/form/SV_cV1yfs9KIQDEEh8)
+            """, unsafe_allow_html=True)
 
- # הוספת קישור אחרי הצגת המשוב
-           st.markdown("""
-    <hr>
-    <a href="https://telavivmedicine.fra1.qualtrics.com/jfe/form/SV_cV1yfs9KIQDEEh8" target="_blank">תודה לכם על ההשתפות בסימולציה, הקליקו פה כדי לענות על שאלון</a>
-""", unsafe_allow_html=True)
-
-    
     
             # Option to restart
            # if st.button("התחל סימולציה חדשה"):
